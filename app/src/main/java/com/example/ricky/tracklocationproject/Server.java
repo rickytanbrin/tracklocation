38f71782024b71c073e45f908aba15765ded303c
Ricky.Tirta <rickyanwart@gmail.com>
2022-04-13 20:23:34 +0700

    init commit

---
+++ app/src/main/java/com/example/ricky/tracklocationproject/Server.java
@@ -0,0 +1,9 @@
+package com.example.ricky.tracklocationproject;
+
+/**
+ * Created by Ricky on 1/3/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://115.85.78.90:8080/MinimalWays/";
+}
---
+++ app/src/main/java/com/example/ricky/tracklocationproject/settings/Constants.java
@@ -0,0 +1,10 @@
+package com.example.ricky.tracklocationproject.settings;
+
+/**
+ * Created by Ricky on 11/29/2017.
+ */
+
+public class Constants {
+    public static final int LOCATION_INTERVAL = 10000;
+    public static final int FASTEST_LOCATION_INTERVAL = 5000;
+}
